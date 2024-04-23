96ca41f41a268507ff54c7457dd46a40e449f656
shilrey <joniesr_tw@yahoo.com.tw>
2024-04-23 23:26:40 +0800

    add command process

--- world.h
+++ world.h
@@ -3,6 +3,7 @@
 #include <map>
 #include <string>
 #include <sstream>
+#include <map>
 #include "prompt.h"
 
 using namespace std;
@@ -27,26 +28,58 @@ struct Object
     }
 };
 
+
 class World : public Object, public CommandReciever
 {
-    void ExecuteCommand(const std::string &cmd)
-    {
-        std::cout << "world checking " << cmd << "..." << std::endl;
+    typedef std::map<std::string, Object> OBJMAP;
+    OBJMAP mapObjects_;
+
+    typedef const std::string (World::*CMD_CALLBACK)(const std::string& param);
+
+    typedef std::map<std::string, CMD_CALLBACK> CMDCBK_MAP;
+
+    CMDCBK_MAP mapCmdCb_;
+
+    const std::string ListItems(const std::string& params) {
+        std::cout << "listing item..." << std::endl;
+        if (!mapObjects_.size())
+            std::cout << "Empty" << std::endl;
+        else
+            for (OBJMAP::iterator i = mapObjects_.begin(); i != mapObjects_.end(); ++i) {
+                std::cout << i->first << ":" << i->second.ToString() << std::endl;
+            }
+        return "complete.";
     }
 
-    std::map<std::string, Object> objects_map_;
-
+    void SetupCmdLists() {
+        mapCmdCb_["listitem"] = &World::ListItems;
+    }
 public:
     World()
     {
+        SetupCmdLists();
         location_ = Location(0, 0);
         std::cout << "-= World Start =-" << endl;
     }
     ~World() {}
 
-    virtual bool OnCommand(const std::string& cmd) {
-        ExecuteCommand(cmd);
-        return true;
+    // 輸入字串，從字串中拆解出command及parameter, 並從mapCmdCb中找出對應的函式來呼叫
+    virtual bool OnCommand(const std::string &line)
+    {
+        std::string cmd, param, result;
+        size_t pos = line.find(' ');
+        if (pos == std::string::npos)
+            cmd = line;
+        else {
+            cmd = line.substr(0, pos++);
+            param = line.substr(pos);
+        }
+        CMDCBK_MAP::iterator i = mapCmdCb_.find(cmd);
+        if (i != mapCmdCb_.end()) {
+            result = (this->*i->second)(param);
+            return true;
+        }
+        return false;
     }
 };
 
