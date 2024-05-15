7768f89b3f513c058f8a0a7080820336dc4e5dd1
shilrey <joniesr_tw@yahoo.com.tw>
2024-05-16 07:05:49 +0800

    add lib def

--- libcworld.h
+++ libcworld.h
@@ -1,6 +1,11 @@
 #ifndef _lib_cworld_declaration_2024_
 #define _lib_cworld_declaration_2024_
 
+extern "C" {
+    typedef const char* (*CALLBACK)(const char*, const char*);
 
+    void InputCommand(const char* cmd);
+    void SetOutputCallback(CALLBACK cb);
+}
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include "rapidjson/document.h"
 #include "world.h"
+#include "libcworld.h"
 
 using namespace std;
 
 extern "C" {
-World w;
+    World w;
 
-int main(int, char* [])
-{
+    int main(int, char* [])
+    {
 
-    w.run();
-	return 0;
-}
+        w.run();
+        return 0;
+    }
+
+    void InputCommand(const char* cmd) {
+        cout << "test " << cmd << endl;
+    }
+    
+    void SetOutputCallback(CALLBACK cb) {
+        cout << "test callback:" << cb << " set" << endl;
+        cout << cb("test", "123");
+    }
 }
